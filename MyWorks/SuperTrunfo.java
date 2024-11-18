4c44d5c2060fa4791a2ffec2eb080f97886b0bfe
big-house842 <gabrielcasagrande681@gmail.com>
2024-11-18 11:48:51 -0300

    - Proncho -

--- MyWorks/SuperTrunfo.java
+++ MyWorks/SuperTrunfo.java
@@ -1,14 +1,15 @@
 package MyWorks;
 
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import Modelo.Ghost;
 
 public class SuperTrunfo {
 
     public static void main(String[] args) {
-        HashMap<Integer, Ghost> gamester1 = new HashMap<>();
-        HashMap<Integer, Ghost> gamester2 = new HashMap<>();
+        Queue<Ghost> gamester1 = new LinkedList<Ghost>();
+        Queue<Ghost> gamester2 = new LinkedList<Ghost>();
 
         Ghost litleGaspar = new Ghost();
         Ghost womenOfMidnight = new Ghost();
@@ -20,46 +21,43 @@ public class SuperTrunfo {
         womenOfMidnight.ReadGhost("six foots", 118, "740");
         matitaPere.ReadGhost("five foots", 519, "1000");
 
-        gamester1.put(1, litleGaspar);
-        gamester1.put(2, theSnot);
+        gamester1.add(litleGaspar);
+        gamester1.add(theSnot);
 
-        gamester2.put(1, womenOfMidnight);
-        gamester2.put(2, matitaPere);
+        gamester2.add(womenOfMidnight);
+        gamester2.add(matitaPere);
 
         fight(gamester1, gamester2);
 
     }
 
-    private static void fight(HashMap<Integer, Ghost> gamester1, HashMap<Integer, Ghost> gamester2) {
+    private static void fight(Queue<Ghost> gamester1, Queue<Ghost> gamester2) {
 
         while (true) {
 
-            Ghost ghost1 = gamester1.get(1);// Acessando os fantasmas de cada jogador
-            Ghost ghost2 = gamester2.get(1);
-
-            if (ghost1 == null) {
+            if (gamester1.peek() == null) {
                 System.out.println(" The gamester two is the best!! ");
-            } else if (ghost2 == null) {
+            } else if (gamester2.peek() == null) {
                 System.out.println(" The gamester one is the best!! ");
             }
 
-            if (ghost1 == null || ghost2 == null) {
+            if (gamester1.peek() == null || gamester2.peek() == null) {
                 break;
             }
 
-            if (Integer.parseInt(ghost1.ReturnStrength()) > Integer.parseInt(ghost2.ReturnStrength())) {
+            if (Integer.parseInt(gamester1.peek().ReturnStrength()) > Integer.parseInt(gamester2.peek().ReturnStrength())) {
 
-                System.out.println("Player 1 win, " + ghost1.ReturnStrength() + " > " + ghost2.ReturnStrength());
-                ghost1win(gamester1, gamester2, ghost1, ghost2);
+                System.out.println("Player 1 win, " + gamester1.peek().ReturnStrength() + " > " + gamester2.peek().ReturnStrength());
+                ghost1win(gamester1, gamester2);
 
-            } else if (Integer.parseInt(ghost1.ReturnStrength()) < Integer.parseInt(ghost2.ReturnStrength())) {
+            } else if (Integer.parseInt(gamester1.peek().ReturnStrength()) < Integer.parseInt(gamester2.peek().ReturnStrength())) {
 
-                System.out.println("Player 2 win, " + ghost2.ReturnStrength() + " > " + ghost1.ReturnStrength());
-                ghost2win(gamester1, gamester2, ghost1, ghost2);
+                System.out.println("Player 2 win, " + gamester2.peek().ReturnStrength() + " > " + gamester1.peek().ReturnStrength());
+                ghost2win(gamester1, gamester2);
 
             } else {
 
-                System.out.println("Tied. The strength of ghosts is the same, " + ghost1.ReturnStrength());
+                System.out.println("Tied. The strength of ghosts is the same, " + gamester1.peek().ReturnStrength());
                 return;
 
             }
@@ -68,25 +66,18 @@ public class SuperTrunfo {
 
     }
 
-    private static void ghost2win(HashMap<Integer, Ghost> gamester1, HashMap<Integer, Ghost> gamester2, Ghost ghost1,
-            Ghost ghost2) {
+    private static void ghost1win(Queue<Ghost> gamester1, Queue<Ghost> gamester2) {
 
-        gamester1.put(gamester2.size() + 1, ghost1);
-
-        gamester2.put(gamester1.size(), null);
-        gamester2.put(1, gamester1.get(2));
-
+        gamester1.add(gamester2.peek());
+        gamester2.poll();
         return;
+
     }
 
-    private static void ghost1win(HashMap<Integer, Ghost> gamester1, HashMap<Integer, Ghost> gamester2, Ghost ghost1,
-            Ghost ghost2) {
+    private static void ghost2win(Queue<Ghost> gamester1, Queue<Ghost> gamester2) {
 
-        gamester1.put(gamester1.size() + 1, ghost2);
-
-        gamester2.put(gamester2.size(), null);
-        gamester2.put(1, gamester2.get(2));
-
+        gamester2.add(gamester1.peek());
+        gamester1.poll();
         return;
     }
 
