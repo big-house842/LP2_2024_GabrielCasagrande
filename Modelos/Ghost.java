6f915f1cdfa679b6ba655d75210f3d7cc97b82d6
big-house842 <gabrielcasagrande681@gmail.com>
2024-11-21 17:02:49 -0300

    - Handler -

--- Modelos/Ghost.java
+++ Modelos/Ghost.java
@@ -5,13 +5,30 @@ public class Ghost {
     private String nameGhost;
     private int strength, size;
 
+
+    
     public void ReadGhost(String a, int s1, int s2){
 
         this.nameGhost = a;
-        this.strength = s2;
-        this.size = s1;
+        this.strength = s1;
+        this.size = s2;
 
     }
+
+
+    public String ReadName(String a){
+        return this.nameGhost = a;
+    }
+
+    public int ReadStrengh(int s1){
+        return this.strength = s1;
+    }
+
+    public int ReadSize(int s2){
+        return this.size = s2;
+    }
+
+
 
     public String ReturnNameGhost(){
         return this.nameGhost;
@@ -25,11 +42,14 @@ public class Ghost {
         return this.size;
     }
 
+
+
     public void PrintGhost(){// imprime o objeto
 
         System.out.print( "   Name _ " + this.nameGhost + "   Strengh _ " + this.strength + "   Size _ " + this.size);
         System.out.println();
 
     }
+
 
 }
---
+++ lib/HandlerFile.java
@@ -0,0 +1,190 @@
+package lib;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Modelos.*;
+
+public class HandlerFile {
+
+    public static void createFile(String filename) {
+        try {
+
+            File fileObj = new File(filename);
+
+            if (fileObj.createNewFile()) {
+                System.out.println("File created: " + fileObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("An error occurred.");
+
+            e.printStackTrace();
+        }
+    }
+
+
+    /*
+     * Para criar um arquivo em um diretório específico (requer permissão),
+     * especifique o caminho do arquivo e use barras invertidas duplas para escapar
+     * o \caractere " " (para Windows).
+     * No Mac e Linux, você pode simplesmente escrever o caminho, como:
+     * /Users/name/filename.txt
+     */
+
+
+    public static void FileWriter(String filename, String msg) {
+        try {
+
+            FileWriter myWriter = new FileWriter(filename);
+
+            myWriter.write(msg);
+
+            myWriter.close();
+
+            System.out.println("Successfully wrote to the file.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+    /*
+     * public static void FileWriterArray(String filename, ArrayList<String>
+     * listaCarro) {
+     * try {
+     * 
+     * FileWriter myWriter = new FileWriter(filename);
+     * 
+     * myWriter.write("Lista: " + listaCarro);
+     * 
+     * myWriter.close();
+     * 
+     * System.out.println("Successfully wrote to the file.");
+     * 
+     * } catch (IOException e) {
+     * System.out.println("An error occurred.");
+     * e.printStackTrace();
+     * }
+     * }
+     */
+
+
+    public static void FileWriter(String filename, ArrayList<Ghost> phantoms) {
+        try {
+
+            FileWriter myWriter = new FileWriter(filename);
+
+            for (Ghost p : phantoms) {
+                myWriter.write(p.ReturnNameGhost() + "\n");
+                myWriter.write("Strengh: " + p.ReturnStrength() + "\n");
+                myWriter.write("Size: " + p.toString() + "\n");
+            }
+
+            myWriter.close();
+
+            System.out.println("Successfully wrote to the file.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static boolean fileExists(String filename) {
+        File myObj = new File(filename);
+        return myObj.exists();
+    }
+
+
+    public static void GetFileInfo() {
+
+        File myObj = new File("arquivo.txt");
+
+        if (myObj.exists()) {
+            System.out.println("_____________________________________");
+            System.out.println("File name: " + myObj.getName());
+            System.out.println("Absolute path: " + myObj.getAbsolutePath());
+            System.out.println("Writeable: " + myObj.canWrite());
+            System.out.println("Readable " + myObj.canRead());
+            System.out.println("File size in bytes " + myObj.length());
+            System.out.println("_____________________________________");
+        } else {
+            System.out.println("The file does not exist.");
+        }
+    }
+
+
+    public static void deleteFile(String filename) {
+
+        File myObj = new File(filename);
+        File myPath = new File(myObj.getAbsolutePath());
+
+        if (myPath.delete()) {
+            System.out.println("Deleted the file: " + myPath.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
+
+    }
+
+
+    public static void ReadFile(String filename) {
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static ArrayList<Ghost> ReadFileA(String filename, ArrayList<Ghost> phantoms) {
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            
+            ArrayList<Ghost> retorno = new ArrayList<>();
+
+            while (myReader.hasNextInt()) {
+                Ghost g = new Ghost();
+                g.ReadName(myReader.next());
+                g.ReadStrengh(myReader.nextInt());
+                g.ReadSize(myReader.nextInt());
+                retorno.add(g);
+            }
+
+        
+            myReader.close();
+            
+            return retorno;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    
+        return null;
+    }
+
+
+}
